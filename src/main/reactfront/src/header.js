3786e342a126a7bea6bee32cfcc92a3d37688871
이상민 <>
2024-02-15 16:49:07 +0900

    commit test

--- src/main/reactfront/src/header.js
+++ src/main/reactfront/src/header.js
@@ -11,10 +11,14 @@ const Header = () => {
   const moveCommunity = () => {
     navigate('/community');
   };
+  const moveCourse = () => {
+    navigate('/course');
+  };
 
   return (
     <header className="header-container">
         <div onClick={moveHome}>Home</div>
+        <div onClick={moveCourse}>course</div>
         <div onClick={moveCommunity}>community</div>
     </header>
   );
--- src/main/reactfront/src/index.js
+++ src/main/reactfront/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './header';
 import Footer from './footer';
 import Home from './routers/home/home';
 import CommunityPage from './routers/CommunityPage/communityPage';
+import Test from './routers/Test/test'
+import CoursePage from './routers/CoursePage/coursePage';
 
 
 
@@ -16,9 +17,10 @@ root.render(
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/community/*" element={<CommunityPage />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/course/*" element={<CoursePage/>}/>
     </Routes>
     <Footer />
   </Router>
 );
 
-reportWebVitals();
