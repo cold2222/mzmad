660058eca399b1f4acc79fe2a08c595c068649c8
이상민 <>
2024-02-26 16:05:33 +0900

    Lecture 등록

--- src/main/reactfront/src/routers/CoursePage/coursePage.jsx
+++ src/main/reactfront/src/routers/CoursePage/coursePage.jsx
@@ -1,12 +1,15 @@
 // LecturePage.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // eslint-disable-next-line
 import styles from './LecturePage.module.css';
-import { useNavigate } from 'react-router-dom';  // Navigation을 위해 useHistory를 import
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const LecturePage = () => {
   const [category, setCategory] = useState('전체');
   const navigate = useNavigate();  // Navigation을 위한 history 객체 생성
+  const [lectureData,setLectureData] = useState([]);
+  const [uniqueCategories, setUniqueCategories] = useState([]);
 
   const handleCategoryChange = (newCategory) => {
     setCategory(newCategory);
@@ -16,48 +19,66 @@ const LecturePage = () => {
     navigate('/admin-registration');  // AdminRegistration 컴포넌트로 이동
   };
 
-  const lectureData = [
-    { id: 1, title: '기초 교양1', category: '교양' },
-    { id: 2, title: '기초 교양2', category: '교양' },
-    { id: 3, title: '기초 교양3', category: '교양' },
-    { id: 4, title: '기초 철학1', category: '철학' },
-    { id: 5, title: '기초 철학2', category: '철학' },
-    { id: 6, title: '기초 철학3', category: '철학' },
-    { id: 7, title: '기초 정치1', category: '정치' },
-    { id: 8, title: '기초 정치2', category: '정치' },
-    { id: 9, title: '기초 정치3', category: '정치' },
-  ];
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    axios.get('/get-course-data') // URL을 적절히 조정하세요.
+      .then(response => {
+        console.log(response)
+        setLectureData(response.data); 
+        extractUniqueCategories(response.data);
+      })
+      .catch(error => {
+        console.error('데이터를 가져오는 데 실패했습니다:', error.message);
+      });
+  };
+
+  const extractUniqueCategories = (data) => {
+    const uniqueCategories = [...new Set(data.map(lecture => lecture.courses_category))];
+    setUniqueCategories(uniqueCategories);
+  };
 
   const filteredLectures = category === '전체'
     ? lectureData
-    : lectureData.filter(lecture => lecture.category === category);
+    : lectureData.filter(lecture => lecture.courses_category === category);
 
-    return (
-      <div className={styles.container}>
-        <div>
-          <button className={styles.adminButton} onClick={handleAdminRegistration}>
-            관리자 등록
-          </button>
-  
-          <label className={styles.label}>카테고리 선택:</label>
-          <select className={styles.select} value={category} onChange={(e) => handleCategoryChange(e.target.value)}>
-            <option value="전체">전체</option>
-            <option value="교양">교양</option>
-            <option value="철학">철학</option>
-            <option value="정치">정치</option>
-          </select>
-        </div>
-  
-        <div>
-          <h2 className={styles.title}>{category} 카테고리의 강의 목록</h2>
-          <ul className={styles.list}>
-            {filteredLectures.map(lecture => (
-              <li key={lecture.id} className={styles.item}>{lecture.title}</li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className={styles.container}>
+      <div>
+        <button className={styles.adminButton} onClick={handleAdminRegistration}>
+          관리자 등록
+        </button>
+
+        <label className={styles.label}>카테고리 선택:</label>
+        <select className={styles.select} value={category} onChange={(e) => handleCategoryChange(e.target.value)}>
+          <option value="전체">전체</option>
+          {uniqueCategories.map(categoryOption => (
+            <option key={categoryOption} value={categoryOption}>{categoryOption}</option>
+          ))}
+        </select>
       </div>
-    );
-  };
-  
-  export default LecturePage;
+
+      <div>
+        <h2 className={styles.title}>{category} 카테고리의 강의 목록</h2>
+        {lectureData.length === 0 ? (
+            <p>Loading...</p>
+          ) : (
+            <ul className={styles.list}>
+              {filteredLectures.map(lecture => (
+                <li key={lecture.courses_date} className={styles.item}>
+                <span>{lecture.courses_name}</span><br/>
+                <video width="320" height="240" controls>
+                  <source src={`LectureVideo/${lecture.courses_video}`} type="video/mp4" />
+                </video>
+              </li>
+              ))}
+            </ul>
+          )}
+      </div>
+    </div>
+  );
+};
+
+export default LecturePage;
